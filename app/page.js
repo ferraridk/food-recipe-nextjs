8565e2bdda5a502645f9c2ca38b7ca6939f6d380
ferraridk <henrikferrari@gmail.com>
2024-04-29 21:03:49 +0200

    Some css and some tweeks for the main header

--- app/page.js
+++ app/page.js
@@ -1,14 +1,50 @@
-import Link from "next/link";
+import Link from 'next/link';
+
+import classes from './page.module.css';
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Share Meals</Link></p>
-      <p><Link href="/community">Community</Link></p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Food 4 You for Next Level Food</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            Food 4 You is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            Food 4 You is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why Food 4 You?</h2>
+          <p>
+            Food 4 You is a platform for food lovers to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            Food 4 You is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
