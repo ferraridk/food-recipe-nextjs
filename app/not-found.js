5ed8f639855e65d2ce0bb5efd3ab22a60930c51a
ferraridk <henrikferrari@gmail.com>
2024-05-29 15:34:22 +0200

    Individual meal throw not found

--- app/not-found.js
+++ app/not-found.js
@@ -1,7 +1,7 @@
 export default function NotFound() {
  return <main className="not-found">
     <h1>
-        Not Found!
+        Meal Not Found!
     </h1>
     <p>
         Sorry. Page Or Resource Not Found.
