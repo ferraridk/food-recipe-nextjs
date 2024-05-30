63050109785e63b00484d08f937feb2c4e85b7fe
ferraridk <henrikferrari@gmail.com>
2024-05-30 13:47:50 +0200

    Adding Server-Side Input Validation

---
+++ app/meals/share/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Error({error}) {
+    return <main className="error">
+        <h1>
+            An Error Occurred!
+        </h1>
+        <p>
+           Failed To Create Meal!
+        </p>
+    </main>
+}
